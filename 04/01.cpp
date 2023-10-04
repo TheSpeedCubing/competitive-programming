e92c2d4acf82997133a710265453725001e5ab92
speedcubing <speedcubingthebest@gmail.com>
2023-10-04 21:42:09 +0800

    update

--- 04/01.cpp
+++ 04/01.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 int main() {
 	int head,data;
-	scanf("%d%d",&head,&data);
-    while(data != head){
-    	printf("%d\n",data);
-    	scanf("%d", &data);
-    }
+	scanf("%d",&head);
+	do{
+		scanf("%d",&data);
     printf("%d\n",data);
+	} while(data != head);
 }
