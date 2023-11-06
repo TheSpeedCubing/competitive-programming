c3f1b6362ce9793fa8402f3b584bdcafac6d559f
speedcubing <speedcubingthebest@gmail.com>
2023-11-06 19:25:07 +0800

    Update 10.cpp

--- CP/13/10.cpp
+++ CP/13/10.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
+#include <bitset>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
-	int n;
-	
-	while(cin >> n && n){
-		
-		 int a[n], k = n, i = 0, cnt = 0;
-		 
-		 for(;i<n;i++)
-		 	  cin >> a[i];
-		 	  
-		 while(k--)
-		 	  for(int j = k+1;j<n;j++)
-		 	  	if(a[k] > a[j])
-		 	  		  cnt++;
-		 	  		  
-		 cout << cnt<<"\n";
-	}
+int main() {
+	 int a,b,flag=1;
+	 while(cin >> a >> b){
+	 	if(flag)
+	 	  flag=0;
+	 	else cout <<"\n";
+	 	   for(int i = 0;i<pow(2,a);i++){
+	 	   	   string s = bitset<16>(i).to_string().substr(16 - a);
+	 	   	   int copy = a , cnt = 0;
+	 	   	   while(copy--)
+	 	   	   	 cnt += s[copy] - 48;
+	 	   	   if(cnt==b)
+	 	   	     cout << s <<"\n";
+	 	   }
+	 }
 }
