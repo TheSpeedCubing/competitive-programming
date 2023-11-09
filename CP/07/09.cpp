a9e3c5bbf786b8d739827e5b429746ffc610171b
speedcubing <speedcubingthebest@gmail.com>
2023-11-09 10:10:29 +0800

    Update 09.cpp/

--- CP/07/09.cpp
+++ CP/07/09.cpp
@@ -11,7 +11,7 @@ double b(int x){
 
 int main() {
     int n,x1, y1, x2, y2;
-	 char hor,ver,hor2,ver2;
+	  char hor,ver,hor2,ver2;
     cin >> n;
     while(n--){
     	  cin >> hor >> x1 >> ver >>y1>>hor2>>x2>>ver2>>y2;
@@ -19,6 +19,6 @@ int main() {
         if(ver==83) y1 = -y1;
         if(hor2==87) x2 = -x2;
         if(ver2==83) y2 = -y2;
-        printf("%d\n",(int) floor(abs(6000*acos(a(y1)*a(y2)+b(y1)*b(y2)*(a(x1)*a(x2)+b(x1)*b(x2))))));
+        printf("%d\n",(int) abs(6000*acos(a(y1)*a(y2)+b(y1)*b(y2)*(a(x1)*a(x2)+b(x1)*b(x2)))));
     }
 }
