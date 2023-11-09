de5ebed18227c81aabbcca8c82b30988b6ba2bfc
speedcubing <speedcubingthebest@gmail.com>
2023-11-09 10:15:28 +0800

    Update 07.cpp

--- CP/08/07.cpp
+++ CP/08/07.cpp
@@ -10,17 +10,21 @@ int main()
 		if(first)
 		   first=0;
 		else printf("\n");
+		
 	  cin >> heal;
-  	int mm[heal+1]={0},in,movement=0,min=2147483647,i=0;
+  	int mm[heal+1]={0},
+  	in,movement=0,
+  	min=2147483647,i=0;
 	  for(;i<heal;i++){
 		  cin >>in;
 		  mm[i+1] = in;
 	  	movement += in;
-	  	min = movement < min?movement:min;
+	  	min = movement < min ? movement : min;
 	  }
 	  min = min < 0 ? -min : 0;
 	  for(i=0;i<heal+1;i++){
-	  	for(int j=0 ; j<min;j++)
+	  	int j =min;
+	  	while(j--)
 	  		printf("-");
 	  	min += mm[i+1];
 	  	cout << name <<"\n";
