ae350ef5fe137bf1fb8e59a66d8e01d1fec680a2
speedcubing <speedcubingthebest@gmail.com>
2023-11-09 10:25:20 +0800

    Update 02.cpp

--- CP/09/02.cpp
+++ CP/09/02.cpp
@@ -4,19 +4,20 @@
 
 using namespace std;
 int J(int n,int m,int i) {
-	return (i == 1 ? (m-1+n) : J(n-1,m,i-1)+m) % n;
+	return (i == 1 ? (m-1+n) : J(n-1,m,i-1)+m)
+	% n;
 }
 int main()
 {
     int n,m,c;
     while(cin >> n >> m >> c) {
       vector<int> arr;
-      
-      for(int i = n-c+1;i<=n;i++)
+      int i = n-c+1;
+      for(;i<=n;i++)
     	  arr.push_back(J(n,m,i)+1);
     	  
       sort(arr.begin(), arr.end());
-      for (int i = 0; i < c; i++)
+      for (i = 0; i < c; i++)
         cout << arr.at(i) << ((i==c-1) ? "\n":" ");
   }
 }
