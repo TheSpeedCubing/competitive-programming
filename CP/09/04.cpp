d8fcc20b0c51cc096b42ad3584ac95b9d970d701
speedcubing <speedcubingthebest@gmail.com>
2023-11-09 10:30:02 +0800

    Update 04.cpp

--- CP/09/04.cpp
+++ CP/09/04.cpp
@@ -4,26 +4,29 @@ using namespace std;
 
 int main()
 {
-	int yy,m1,m2,flag=1;
-	while(cin >> yy >> m1 >> m2){
+	int yy,mm,m2,flag=1;
+	while(cin >> yy >> mm >> m2){
 		
 		if(flag)
 		  flag=0;
 		else cout <<"\n";
 		
 		int data[] = {-1,31,28,31,30,31,30,31,31,30,31,30,31};
-		data[2] += (yy%4 ? (yy%100 ? !yy%400 : 0) : 1);
 		
-		for(int mm = m1;mm <= m2;mm++){
+		
+		data[2] += (!(yy % 4) && yy % 100) || !(yy % 400);
+		
+		while(mm <= m2){
 	     for(int dd = 1;dd<=data[mm]; dd++){
-	     	  int y = yy - (mm <3);
-	     	  int m = mm + (mm < 3 ? 10 : -2);
-	     	  int c = y / 100;
+	     	  int y = yy - (mm <3),
+	     	      m = mm + (mm < 3 ? 10 : -2),
+	     	      c = y / 100;
 	     	  y%=100;
 	     	  int w = (int) (dd+floor(2.6*m-0.2)+y+floor(y/4)+floor(c/4)-2*c) %7;
 	     	  if(w == -1 || w == 0 || w == 6)
 	     	    printf("%02d/%02d/%04d\n",dd,mm,yy);
 	     }
+	     mm++;
 		}
 	}
 }
