5587d20848de98d904cd9b65ac9aeccdf2af8bd3
speedcubing <speedcubingthebest@gmail.com>
2023-11-09 10:41:39 +0800

    Update 09.cpp

--- CP/09/09.cpp
+++ CP/09/09.cpp
@@ -14,15 +14,21 @@ int main() {
       int calc = s[0], t, i = 1;
       for (; i < 5; i++) {
         t = op[i - 1];
-        calc = t == 0 ? calc + s[i] : (t == 1 ? calc - s[i] : (t == 2 ? calc * s[i] : (s[i] == 0 ? -2147483648 : calc / s[i])));
+        calc = t ? (t == 1 ? calc - s[i]
+                           : (t == 2 ? calc * s[i] 
+                                     : (s[i] ? calc / s[i]
+                                             : -2147483648)))
+                 : calc + s[i];
       }
       if (f == calc) {
 
         for (i = 0; i < 4; i++)
-          cout << (op[i] == 0 ? "+" : (op[i] == 1 ? "-" : (op[i] == 2 ? "*" : "/")));
+          cout << (op[i] ? (op[i] == 1 ? "-" : (op[i] == 2 ? "*" : "/")) : "+");
         cout << "\n";
         break;
+        
       } else {
+        
         op[3]++;
         i = 3;
         while (i--) {
@@ -31,6 +37,7 @@ int main() {
             op[i]++;
           }
         }
+      
       }
     }
     if (op[0] == 4)
