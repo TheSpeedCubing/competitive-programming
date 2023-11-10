9da71341ac23ea4b93237c0d62f69439c6bc9123
speedcubing <speedcubingthebest@gmail.com>
2023-11-10 20:36:31 +0800

    Update 03.cpp

--- CP/07/03.cpp
+++ CP/07/03.cpp
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <iostream>
 
 int main() {
-	double a=0,c;
-	int b=0,d;
-    while (scanf("%lf %d", &c, &d) != EOF) {
-    	a+=c; b+=d;
+	double a=0,b=0,c,d;
+    while (std::cin >> c >> d) {
+    	a+=c; 
+    	b+=d;
     }
     printf("%.2f", b/a);
 }
