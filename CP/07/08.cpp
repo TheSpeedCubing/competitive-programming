67dcbaad6f6c714a5ec3eceeee4a99bc3db98ed5
speedcubing <speedcubingthebest@gmail.com>
2023-11-10 20:32:33 +0800

    Update 08.cpp

--- CP/07/08.cpp
+++ CP/07/08.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main() {
     double val;
     while (cin >> val) {
+    	  if(val == 0)
+    	    cout << "0\n";
+    	  else {
         int fe = floor(log10(abs(val))), flag = fe >= 0;
         cout << (flag ? "+" : "") << (flag ? fe + 1 : fe) << "\n";
+      }
     }
 }
