258051556dd27550f6e832e2cd14862cc9e8f58f
speedcubing <speedcubingthebest@gmail.com>
2023-11-13 21:58:11 +0800

    Update 02.cpp

--- CP/07/02.cpp
+++ CP/07/02.cpp
@@ -1,38 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    double x, A;
+  	double x;
     unsigned long long b;
     unsigned long long y;
-    while (scanf("%lf", & x) != EOF) {
-        A = fabs(x), b = 1023,y = 0;
-
-        while (A >= 2) {
-            b++;
-            A /= 2;
-        }
-
-        while (A < 1) {
-            b--;
-            A *= 2;
-        }
-        A -= 1;
-        
-        int n = 52;
-        while (n--) {
-            b *= 2;
-            A *= 2;
-            y *= 2;
-            if (A >= 1) {
-                y++;
-                A--;
-            }
-        }
-        
-        y += b;
-        if (x < 0)
-            y += 9223372036854775808;
+    while (scanf("%lf", &x) != EOF) {
+    	  y = *(long long *) &x;
         printf("%g %g %llu\n", x, y, y);
     }
 }
