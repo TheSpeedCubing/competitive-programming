25aec1418d9ac7a7d9e789c6a41160a4bb59b7d6
speedcubing <speedcubingthebest@gmail.com>
2023-11-13 21:39:26 +0800

    Update 05.cpp

--- CP/07/05.cpp
+++ CP/07/05.cpp
@@ -1,23 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    double locx[4], locy[4];
     while (1) {
-        int a = 0, b = 1, c = 2,i=0;
-    	  for(;i<4;i++){
-    	  	if(scanf("%lf %lf",&locx[i],&locy[i]) == EOF)
+        double locx[4], locy[4];
+        int i=0;
+        while(i<4) {
+    	  	if (!(cin >> locx[i] >> locy[i++]))
     	  	  return 0;
-    	  }
-        if (locx[0] == locx[2] && locy[0] == locy[2]) {
-            c = 3;
-        } else if (locx[1] == locx[2] && locy[1] == locy[2]) {
-            a = 1;
-            b = 0;
-            c = 3;
-        } else if (locx[1] == locx[3] && locy[1] == locy[3]) {
-            a = 1;
-            b = 0;
         }
-        printf("%.3f %.3f\n", locx[b] + locx[c] - locx[a], locy[b] + locy[c] - locy[a]);
+        
+    	  int t2 = locx[1] == locx[2] && locy[1] == locy[2],
+    	      a = (locx[1] == locx[3] && locy[1] == locy[3]) || t2,
+    	      c = ((locx[0] == locx[2] && locy[0] == locy[2]) || t2) + 2;
+    	      
+        printf("%.3f %.3f\n", locx[!a] + locx[c] - locx[a], locy[!a] + locy[c] - locy[a]);
     }
 }
