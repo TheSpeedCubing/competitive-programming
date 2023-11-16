1c8f87a880663517249158aae05880abb1959d09
speedcubing <speedcubingthebest@gmail.com>
2023-11-16 19:12:27 +0800

    Update 01.cpp

--- CP/09/01.cpp
+++ CP/09/01.cpp
@@ -5,21 +5,20 @@
 using namespace std;
 
 int main() {
-	char c;
+	int c;
 	while(cin >> c) {
-		 cin.ignore();
 		 string s; 
 		 long long i = 0;
-		 if(c == '1'){
-		 	 cin >> i;
-		 	 cin.ignore();
+		 if(c == 1) {
+		 	  cin >> i;
+		 	  cin.ignore();
         while (i) {
             s = (char) (i % 16 + 65) + s;
             i >>= 4;
         }
         cout << s <<"\n";
 		 } else {
-		   getline(cin,s);
+		 	 cin >> s;
 		   int t = s.length();
 		   while(t--)
 		     i += (s[s.length()-t-1] - 'A') * pow(16,t);
