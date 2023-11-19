0d8ec68534a02f966c05ee2ffaa014cf96dafe01
speedcubing <speedcubingthebest@gmail.com>
2023-11-19 20:19:01 +0800

    Update 04.cpp

--- CP/12/04.cpp
+++ CP/12/04.cpp
@@ -17,15 +17,17 @@ int main()
   	   
   	   mm = -mm;
   	   
-	    int y = yy - (mm <3);
-	    int m = mm + (mm < 3 ? 10 : -2);
-	    int c = y / 100;
-	    y%=100;
+	    int data[] = {0,31,28+((!(yy % 4) && yy % 100) || !(yy % 400))
+		,31,30,31,30,31,31,30,31,30,31},
+		
+		  y = yy - (mm <3),
+	    m = mm + (mm < 3 ? 10 : -2),
+	    c = y / 100;
+	    
+	    y %=100;
+	    
 	    int w = (int) (1+floor(2.6*m-0.2)+y+floor(y/4)+floor(c/4)-2*c) %7;
 	    w = w < 0 ? w + 7 : w;
-	    
-	    int data[] = {-1,31,28,31,30,31,30,31,31,30,31,30,31};
-	    data[2] += (!(yy % 4) && yy % 100) || !(yy % 400);
 	    
 	    cout << month[mm] <<", "<<yy<<"\nSun Mon Tue Wed Thr Fri Sat\n";
 	    
