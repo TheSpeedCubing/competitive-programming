5cdfcda8faec1e409f6529c6e8717325980ab846
speedcubing <speedcubingthebest@gmail.com>
2023-11-28 17:23:29 +0800

    Update 08.cpp

--- CP/12/08.cpp
+++ CP/12/08.cpp
@@ -5,22 +5,22 @@ using namespace std;
 int isBachelorNum(int n)
 {
 	  n = max(-n,n);
-	  bool b[128] = {0};
-	  {
+	  bool b[128] = {};
+	  
 	  	while(n){
 	  		if(b[n%10])
 	  			return 0;
 	  		 b[n%10] = 1;
 	  		 n/=10;
 	  	}
-	  }
+	  	
     return 1;
 }
 
 int main()
 {
 	int i;
-	while(cin >> i){
+	while(cin >> i) {
 		cout << (isBachelorNum(i) ? "Yes\n" : "No\n");
 	}
 }
