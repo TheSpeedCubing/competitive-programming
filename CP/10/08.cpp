771448e2374020ca7ddde92709f0b2951b49cb32
speedcubing <speedcubingthebest@gmail.com>
2023-12-10 22:58:24 +0800

    Update 08.cpp

--- CP/10/08.cpp
+++ CP/10/08.cpp
@@ -1,22 +1,15 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-string s;
-int search(string data){
-	  int c = 0;
-		for(char ch : s) {
-			c+= (data[c] == tolower(ch));
-			if(!data[c])
-			  return 0;
-		}
-		return 1;
-}
-
-int main()
-{
+int main() {
+	string s;
 	while(getline(cin,s)){
-		 cout << (search("cloud") || search("sun") ? "Fail\n" : "Success\n");
+		 int a=0,b=0;
+		 for(char c : s) {
+		 	  a+= "sun"[a] == tolower(c);
+		 	  b+= "cloud"[b] == tolower(c);
+		 }
+		 cout << (a==3 && b == 5? "Success\n" : "Fail\n");
 	}
 }
