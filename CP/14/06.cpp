b1de136da24c2d9477d5a606fb2abf7e584db2c0
speedcubing <speedcubingthebest@gmail.com>
2023-12-20 00:59:09 +0800

    Update 06.cpp

--- CP/14/06.cpp
+++ CP/14/06.cpp
@@ -5,23 +5,22 @@ using namespace std;
 	
 int main() {
   bool cache[1000001] = {0};
-	int T,n,w;
+	int T;
 	cin >> T;
-	while(T--) {
+	while(T--){
+	int n,w;
 	cin >> n >> w ;
-	
-		int max = -2147483648,
-		    maxP = 0,
-		    lastN = 0,
-		    t, 
-		    last = 0,
-		    i = 0;
-		for (;i<n;i++) {
+		int max = -2147483648;
+		int maxP = 0;
+		int lastN = 0,firstN = 0;
+		int t = 0, last = 0;
+		for (int i = 0;i<n;i++) {
 			cin >> t;
-			
-			if(i>0)
-				 cache[i-1] = ((last < 0 && t > 0) || (last > 0 && t < 0)); 
-			
+			if(i>0) {
+				 int s1 = last;
+				 int s2 = t;
+				 cache[i-1] = ((s1 < 0 && s2 > 0) || (s1 > 0 && s2 < 0)); 
+			}
 			last = t;
 			if(i>=w-1) {
 				  lastN += (cache[i-1] - cache[i-w]);
