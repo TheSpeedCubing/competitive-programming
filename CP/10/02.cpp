34576cf4eaa642be0fd0458ec27febeeecb5cbad
speedcubing <speedcubingthebest@gmail.com>
2023-12-20 00:53:51 +0800

    Update 02.cpp

--- CP/10/02.cpp
+++ CP/10/02.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
 	int c,k,m,l;
 	while(cin >> c >> k >> m >> l){
-		int cp = l;
 		vector<int> s;
 		while(1) {
 		  l = (c * l + k)%m;
 		  auto t2 = find(s.begin(),s.end(),l);
-		  int t = t2 != s.end();
-		  if(cp == l || t) {
-		  		cout <<(s.size() - (t ? t2-s.begin():-1)) <<"\n";
+		  if(t2 != s.end()) {
+		  		cout << (s.size() - (t2 - s.begin())) <<"\n";
 		  		break;
 		  }
 		  s.push_back(l);
