669871ba4b39d01f09ee6153aba2cd3eea47ee51
speedcubing <speedcubingthebest@gmail.com>
2023-12-20 01:13:21 +0800

    Update 08.cpp

--- CP/13/08.cpp
+++ CP/13/08.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-void pr(int arr[], int data[], int start, int end, int index) {
+int arr[13],dt[13];
+void pr(int start, int end, int index) {
     if (index == 6) {
         for (int i = 0; i < 6; i++)
-            cout << data[i] << (i == 5 ?"\n": " ");
+            cout << dt[i] << (i == 5 ?"\n": " ");
     } else 
       for (int i = start; i <= end && end - i + 1 >= 6 - index; i++) {
-        data[index] = arr[i];
-        pr(arr, data, i + 1, end, index + 1);
+        dt[index] = arr[i];
+        pr(i + 1, end, index + 1);
     }
 }
 
@@ -20,13 +20,13 @@ int main()
 		if(flag)
 		  flag =  0;
 		else cout <<"\n";
-		int arr[n], d , data[n], i = 0;
+		int d, i = 0;
 		for(;i<n;i++){
 		  cin >> d;
 		  arr[i] = d;
 		}
 		sort(arr,arr+n);
-		pr(arr,data,0,n-1,0);
+		pr(0,n-1,0);
 	}
 }
 
