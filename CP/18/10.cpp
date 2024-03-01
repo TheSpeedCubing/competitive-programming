3e9c59b7d0f8e06d0e7ac2ab4ff92c7ed52092d8
speedcubing <speedcubingthebest@gmail.com>
2024-03-01 20:21:22 +0800

    Update 10.cpp
    code misplace

--- CP/18/10.cpp
+++ CP/18/10.cpp
@@ -1,23 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-	int n;
-	
-	while(cin >> n && n){
-		
-		 int a[1024], k = n, i = 0, cnt = 0;
-		 
-		 for(;i<n;i++)
-		 	  cin >> a[i];
-		 	  
-		 while(k--)
-		 	  for(int j = k+1;j<n;j++)
-		 	  	if(a[k] > a[j])
-		 	  		  cnt++;
-		 	  		  
-		 cout << cnt<<"\n";
-	}
+int main() {
+	string s;
+	while(cin >> s){
+		int result = 1;
+		for(int i = 1;i<=s.length()/2;i++) {
+			 if(!(s.length()%i)) {
+			 	  bool f = true;
+			 	  int j = i;
+			 	  while(j--) {
+			 	  	 int k = i;
+			 	  	 while(k<s.length() && f) {
+			 	  	 	 f = f && s[j] == s[j+ k];
+			 	  	 	 k+=i;
+			 	  	 }
+			   }
+			   if(f) {
+			   	  result = s.length()/i;
+			 	  	break;
+			   }
+		  }
+	  }
+	  cout <<result<<"\n";
+  }
 }
