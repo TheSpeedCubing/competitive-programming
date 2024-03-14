b97f8621d426d999375d858e7aa47bc987aa10c4
speedcubing <speedcubingthebest@gmail.com>
2024-03-15 02:49:52 +0800

    Update 03.cpp

--- CP/13/03.cpp
+++ CP/13/03.cpp
@@ -1,36 +1,29 @@
-#include <iostream>
-#include <cmath>
-#include <sstream>
-#include <string>
-using namespace std;
-int main()
-{
-	int n,flag=1;
-	while(cin >> n){
-		if(flag)
-      flag =0;
-    else cout <<"\n";
-    
-		string s = " *\n * *\n   *", mid=" *";
-		
-		for(int i = 2;i<=n;i++){
-			int t = pow(2,i);
-		    string s2 = "", s3;
-		    istringstream ss(s);
-		    
-		    while(getline(ss,s3,'\n')) {
-		        int r = t;
-		        while(r--)
-		            s2+=" ";
-		        s2+=s3+"\n";
-		    }
-		    s+="\n";
-		    
-		    while(t--)
-		        s+=mid;
+#include <bits/stdc++.h>
 
-		    s+="\n"+s2.substr(0,s2.length()-1);
+using namespace std;
+
+string s[] = {" *"," * *","   *"};
+
+void f(int n, int a) {
+	if(n == 1) {
+		for(string ss : s) {
+			cout << setw(a) << "" << ss <<"\n";
 		}
-		cout << s<<"\n";
+	} else {
+		f(n-1,a);
+		cout << setw(a) << "";
+		int i = pow(2,n);
+		while(i--)
+		  cout << " *";
+		cout <<"\n";
+    f(n-1,a+pow(2,n));
+	}
+}
+int main() {
+	int n,flag=0;
+	while(cin >> n) {
+		if(flag++)
+      cout <<"\n";
+    f(n,0);
 	}
 }
