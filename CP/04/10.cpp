5f6e217ed12daea12dbee88fe4a058541a0c3842
speedcubing <speedcubing@speedcubing.top>
2024-09-28 18:17:08 +0800

    Update 10.cpp
    someone asked

--- CP/04/10.cpp
+++ CP/04/10.cpp
@@ -1,45 +1,51 @@
 #include <stdio.h>
 
-int d[4],i,die;
-int f(int k) {
-	int ori = 0;
-	for(int i = 0;i<4;i++)
-		  if(d[i]==k)
-			    ori++;
-  return ori;
-}
-int main()
-{
-    scanf("%d", &die);
-    while(die) {
-      	int sum=0,k=0,xx[6],s=0;
-      	for(i = 0;i<4;i++){
-    		    d[i] = die%10;
-    		    sum += d[i];
-    		    die /= 10;
-    	  }
-        bool t = false, alr = false;
-        for(i =0;i<6;i++) {
-        	  s+=2;
-        	  xx[i] = f(i+1);
-        	  t = (t||xx[i]==3);
-        	  if(xx[i])
-        	      k++;
-        	  if(xx[i]==2 && !alr){
-          	   sum-=s;
-      	       alr=true;
-        	  }
-        }
-        if(k==4||t) 
-            printf("-\n");
-        else {
-            if(k==1 || sum == 12) 
-                printf("SiPaLa\n");
-            else if(sum==3) 
-                printf("BG\n");
-            else
-                printf("%d\n", sum);
-        }
-        scanf("%d", &die);
+int main() {
+  while (1) {
+
+    int arr[4];
+    int count[7] = {};
+    int maxcount = 0;
+    int in ;
+    for (int i = 0; i < 4; i++) {
+      scanf("%1d", & in);
+      if (in == 0) {
+        return 0;
+      }
+      count[in]++;
+      arr[i] = in;
+      maxcount = maxcount < count[in] ? count[in] : maxcount;
     }
+
+    if (maxcount == 3 || maxcount == 1) {
+      printf("-\n");
+    } else if (maxcount == 2) {
+      bool hasunique = false;
+      for (int i : count) {
+        hasunique = hasunique || (i == 1);
+      }
+
+      int result = 0;
+        for (int i = 1; i <= 6; i++) {
+          if(hasunique) {
+            if (count[i] == 1) {
+              result += i;
+            }
+          } else {
+            if (count[i] == 2) {
+              result = i*2 > result ? i*2 : result;
+            }
+        	}
+        }
+      if (result == 12) {
+        printf("SiPaLa\n");
+      } else if (result == 3) {
+        printf("BG\n");
+      } else {
+        printf("%d\n", result);
+      }
+    } else {
+      printf("SiPaLa\n");
+    }
+  }
 }
