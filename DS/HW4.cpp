755f0fbd789aab51f91e548abd3b8777fee19f32
speedcubing <speedcubing@speedcubing.top>
2024-09-30 17:24:10 +0800

    Update HW4.cpp

--- DS/HW4.cpp
+++ DS/HW4.cpp
@@ -1,21 +1,15 @@
-// HW4.cpp :  w q D   x   ε{     i J I C
+// HW3.cpp : �w�q�D���x���ε{�����i�J�I�C
 //
 
 #include <iostream>
 using namespace std;
 
-#define MAX_TERMS 10 /*size of terms array*/
+#define MAX_TERMS 10
 
 typedef struct{
 	int coef;
 	int expo;
 } polynomialTerm;
-
-typedef struct linkedTerm {
-	int coef;
-	int expo;
-	linkedTerm *nextTermPtr;
-} linkedPolynomialTerm;
 
 void clear(polynomialTerm t[]) 
 {
@@ -27,89 +21,53 @@ void clear(polynomialTerm t[])
 	return;
 }
 
-void clear(linkedPolynomialTerm *&polynomialTermPtr) 
+void printArrayBasedPoly(polynomialTerm t[])
 {
-	linkedPolynomialTerm *tmpPtr;
+	if ( t[0].coef == 0 ) 
+		return;
 
-	while ( polynomialTermPtr != nullptr ) {
-		tmpPtr = polynomialTermPtr;
-		polynomialTermPtr = polynomialTermPtr->nextTermPtr;
-		delete tmpPtr;
+	if ( t[0].expo == 0 )
+		cout << t[0].coef;
+	else
+		cout << t[0].coef << "X^" << t[0].expo;
+
+	for ( int i = 1; i < MAX_TERMS; i++ ) {
+		if ( t[i].coef == 0 ) 
+			return;
+
+		if ( t[i].expo == 0 )
+			cout << " + " << t[i].coef;
+		else
+			cout << " + "<< t[i].coef  << "X^" << t[i].expo;
 	}
 }
 
-void inputTerms(polynomialTerm terms[], int coef, int expo)
+void inputTerm(polynomialTerm t[], int coef, int expo)
 {
-
 	// add your code here
     for(int i = 0; i < MAX_TERMS; i++ ) {
-		if(terms[i].expo == expo) {
-			terms[i] = {coef,expo};
+		if(t[i].expo == expo) {
+			t[i] = {coef,expo};
 			if(coef == 0) {
 			    while(i < MAX_TERMS-1) {
-			        terms[i] = terms[i+1];
+			        t[i] = t[i+1];
 			        i++;
 			    }
 			}
 			return;
 		}
-	    if(terms[i].expo < expo) {
+	    if(t[i].expo < expo) {
 	        //insert
-	        if(coef == 0) {
-	            return;
-	        }
+	        if(coef != 0) {
 			for(int j = MAX_TERMS-1;j>i;j--) {
-				terms[j] = terms[j-1];
+				t[j] = t[j-1];
 			}
-			terms[i] = {coef,expo};
-			return;
+			t[i] = {coef,expo};
+		}
+		return;
 	    }
 	}
 }
-
-//custom methods
-void addHelper(linkedPolynomialTerm *&polyPtr, int coef, int expo,bool add) {
-    
-    if (polyPtr == NULL || polyPtr->expo < expo) {
-	    linkedPolynomialTerm *tmpPtr = new linkedPolynomialTerm;
-	    tmpPtr->coef = coef;
-	    tmpPtr->expo = expo;
-        tmpPtr->nextTermPtr = polyPtr;
-        polyPtr = tmpPtr;
-    } else if(polyPtr->expo == expo) { //update value
-        polyPtr->coef = (add ? polyPtr->coef : 0)+ coef;
-    } else { //iterate to next
-        addHelper(polyPtr->nextTermPtr,coef,expo,add);
-    }
-}
-
-void removeZero(linkedPolynomialTerm** n) {
-    if (*n == NULL) 
-        return;
-
-    if ((*n)->coef == 0) {
-        linkedPolynomialTerm* temp = *n;
-        *n = (*n)->nextTermPtr;
-        delete temp;
-        removeZero(n);
-    } else {
-        removeZero(&(*n)->nextTermPtr);
-    }
-}
-
-void inputLinkTerms(linkedPolynomialTerm *&polyPtr, int coef, int expo)
-{
-	linkedPolynomialTerm *tmpPtr;
-
-	tmpPtr = new linkedPolynomialTerm;
-	tmpPtr->coef = coef;
-	tmpPtr->expo = expo;
-	
-	// add your code here
-    addHelper(polyPtr,coef,expo,false);
-    removeZero(&polyPtr);
-}
-
 
 void addArrayBasedPoly(polynomialTerm a[], polynomialTerm b[], polynomialTerm d[])
 {
@@ -145,84 +103,13 @@ void addArrayBasedPoly(polynomialTerm a[], polynomialTerm b[], polynomialTerm d[
 	}
 }
 
-linkedPolynomialTerm *addLinkBasedPoly(linkedPolynomialTerm *aPtr, linkedPolynomialTerm *bPtr)
-{
-	linkedPolynomialTerm *dPtr;
-	// add your code here
-	dPtr = nullptr;
-	while(aPtr != nullptr) {
-	    addHelper(dPtr,aPtr->coef,aPtr->expo,true);
-	    aPtr = aPtr->nextTermPtr;
-	}
-	while(bPtr != nullptr) {
-	    addHelper(dPtr,bPtr->coef,bPtr->expo,true);
-	    bPtr = bPtr->nextTermPtr;
-	}
-
-    removeZero(&dPtr);
-	return dPtr;
-}
-
-void printArrayBasedPoly(polynomialTerm terms[])
-{
-	if ( terms[0].coef == 0 ) 
-		return;
-
-	if ( terms[0].expo == 0 )
-		cout << terms[0].coef;
-	else
-		cout << terms[0].coef << "X^" << terms[0].expo;
-
-	for ( int i = 1; i < MAX_TERMS; i++ ) {
-		if ( terms[i].coef == 0 ) 
-			return;
-
-		if ( terms[i].expo == 0 )
-			cout << " + " << terms[i].coef;
-		else
-			cout << " + "<< terms[i].coef  << "X^" << terms[i].expo;
-	}
-}
-
-void printLinkBasedPoly(linkedPolynomialTerm *polyPtr)
-{
-	linkedPolynomialTerm *termPtr =  polyPtr;
-
-	if ( termPtr  == nullptr )
-			return;
-
-	if ( termPtr->expo == 0 )
-		cout << termPtr->coef;
-	else
-		cout << termPtr->coef << "X^" << termPtr->expo;
-
-	termPtr  = termPtr->nextTermPtr;
-	
-	while ( termPtr  != nullptr ) {
-		if ( termPtr->coef == 0 ) 
-			return;
-
-		if ( termPtr->expo == 0 )
-			cout << " + " << termPtr->coef;
-		else
-			cout << " + "<< termPtr->coef  << "X^" << termPtr->expo;
-
-		termPtr = termPtr->nextTermPtr;
-	}
-}
-
 int main()
 {
 	polynomialTerm a[MAX_TERMS], b[MAX_TERMS], d[MAX_TERMS];
-	linkedPolynomialTerm *aPtr, *bPtr, *dPtr;
-
 	int coef, expo;
-
-	aPtr = bPtr = dPtr = nullptr;
 
 	while (1) {
 		clear( a ); clear( b ); clear( d );
-		clear( aPtr ); clear( bPtr ); clear( dPtr );
 
 		for ( int i = 0; i < MAX_TERMS; i++ ) {
 			cout << "\ninput a's coefficient and exponent: ";
@@ -231,14 +118,11 @@ int main()
 			if ( expo == 0 && coef == 0 )
 				break;
 
-			inputTerms( a, coef, expo );
-			inputLinkTerms( aPtr, coef, expo );
+			inputTerm( a, coef, expo );
 		}
-		
+
 		cout << "\n\na = ";
 		printArrayBasedPoly( a );
-		cout << "\na = ";
-		printLinkBasedPoly( aPtr );
 		cout << "\n";
 		
 		for ( int i = 0; i < MAX_TERMS; i++ ) {
@@ -248,29 +132,19 @@ int main()
 			if ( expo == 0 && coef == 0 )
 				break;
 
-			inputTerms( b, coef, expo );
-			inputLinkTerms( bPtr, coef, expo );
+			inputTerm( b, coef, expo );
 		}
 
 		cout << "\n\nb = ";
 		printArrayBasedPoly( b );
-
-		cout << "\nb = ";
-		printLinkBasedPoly( bPtr );
-
 		cout << "\n";
 
 		// d =a + b, where a, b, and d are polynomials
-
 		addArrayBasedPoly( a, b, d );
 		cout << "\na + b = ";
 		printArrayBasedPoly( d );
-		
-		dPtr = addLinkBasedPoly( aPtr, bPtr );
-		cout << "\na + b = ";
-		printLinkBasedPoly( dPtr );
-
 		cout << "\n";
 	}
+
 	return 0;
 }
