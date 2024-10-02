61dc96b282996b0243e6e8108805c2631133f5d1
speedcubing <speedcubing@speedcubing.top>
2024-10-03 00:28:25 +0800

    Update 04.cpp

--- CP/05/04.cpp
+++ CP/05/04.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  long long n, val,i;
-  scanf("%lld", & n);
-  while (n) {
-  	val = 0,i = n + 1;
-  	while(i--)
-  	  val += i * (n-i+1);
-    printf("%lld\n", val);
-    scanf("%lld", & n);
+  long long n, sum, n2;
+  
+  while (scanf("%lld", &n) && n) {
+    sum = 0;
+    n2 = n;
+    n++;
+  	while(n--) {
+      sum += n * (n2 - n + 1);
+  	}
+    printf("%lld\n", sum);
   }
 }
