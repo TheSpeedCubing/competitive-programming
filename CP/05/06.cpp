1a4013512206d0670ca4b2d0f0121918acb9b1d0
speedcubing <speedcubing@speedcubing.top>
2024-10-02 16:49:44 +0800

    Update 06.cpp

--- CP/05/06.cpp
+++ CP/05/06.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 unsigned b, b2, b3, b4;
-void a() {
-  scanf("%u%u%u%u", & b, & b2, & b3, & b4);
-}
+
 int main() {
-  a();
-  while (b || b2 || b3 || b4) {
+  while (1) {
+    scanf("%u%u%u%u", &b, &b2, &b3, &b4);
+    if(!(b || b2 || b3 || b4)) {
+  		return 0;
+  	}
     printf("%d\n", b4 + (b3 << 8) + (b2 << 16) + (b << 24));
-    a();
   }
 }
