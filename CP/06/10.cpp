980b4d2084dc7ed1df529df7be7c38d5bddf7ac4
speedcubing <speedcubing@speedcubing.top>
2024-10-28 15:34:58 +0800

    Update 10.cpp

--- CP/06/10.cpp
+++ CP/06/10.cpp
@@ -1,29 +1,26 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 
-int rowS, columnS,row,column,i,flag,t;
+using namespace std;
+
+
 int main() {
-    while(scanf("%d%d", &rowS, &columnS)!=EOF){
-        if (flag)
-            printf("\n");
-        else flag = 1;
-        char s[rowS];
-        s[rowS] = NULL;
-        for (i = 0;i<columnS;) {
-            row = i / rowS;
-		        column = i % rowS;
-		        t = rowS - column - 1;
-            s[row % 2 ? t : column] = 97 + i % 26;
-            if (!((++i) % rowS))
-			          printf("%s\n",s);
-		    } 
-        
-        if (columnS % rowS) {
-            if(row % 2) 
-                while(t--)
-                    s[t] = 32;
-            else 
-			          s[column+1] = NULL;
-            printf("%s\n", s);
-        }
+  int w, l, f = 0;
+  while(cin >> w >> l) {
+    if(f++) {
+      cout << "\n";
     }
+    char n[w+1];
+    int c = 0, line = 1;
+    while(l > 0) {
+       memset(n,' ',sizeof(n));
+       int i;
+       for(i = 0; i < w && l; i++) {
+    	 l--;
+         n[line % 2 ? i : w - i - 1] = (c++ % 26 + 'a');
+       }
+       n[line % 2 ? i : w] = '\0';
+       line++;
+       cout << n << "\n";
+    }
+  }
 }
