c447b129933e372a062a4cec9690cb4f73795335
speedcubing <git@speedcubing.top>
2024-11-28 00:07:41 +0800

    Update 01.cpp

--- CP/08/01.cpp
+++ CP/08/01.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main() {
-    char o,buffer[1024],c;
+    string s, o;
+	  
     long long a, b;
-    while(cin.getline(buffer, 1024)){
-        cin >> o >> a >> b>>c;
-        if(!b && o =='/')
+    while (getline(cin, s)) {
+        cin >> o >> a >> b;
+        cin.ignore();
+        if(!b && o == "/")
           cout << "division by zero\n";
-        else cout << (o=='+'?a+b:(o=='-'?a-b:o=='*'?a*b:a/b)) <<"\n";
+        else cout << (o=="+"?a+b:(o=="-"?a-b:o=="*"?a*b:a/b)) <<"\n";
     }
 }
