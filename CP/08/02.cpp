7fd6fdaddeb87b0e03077a56e6dff5d3969be21f
speedcubing <git@speedcubing.top>
2024-11-28 00:11:50 +0800

    Update 02.cpp

--- CP/08/02.cpp
+++ CP/08/02.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int t, cnt, flag=1,i,pos[1024];
-    char buffer[100][1024];
+int f, t, n, i, pos[1024];
+int main() {
+    string s[1024];
     cin >> t;
     while(t--){
-        if(flag) 
-          flag = 0;
-        else printf("\n");
-        cin >>cnt;
-        for(i=0;i<cnt;i++)
-          cin >> pos[i];
-        for(i=0;i<cnt;i++)
-          cin >> buffer[i];
-        for(i=0;i<cnt;i++)
-          cout <<buffer[pos[i]]<<"\n";
+        if(f++) cout <<"\n";
+        
+        cin >> n;
+        
+        i = 0;
+        while(i<n)
+          cin >> pos[i++];
+        
+        i = 0;
+        while(i<n)
+          cin >> s[i++];
+        
+        i = 0;
+        while(i<n)
+          cout <<s[pos[i++]]<<"\n";
     }
 }
