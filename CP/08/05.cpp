f7eac52a229185be343be0cec2bc898a62cd6cec
speedcubing <git@speedcubing.top>
2024-11-28 00:18:11 +0800

    Update 05.cpp

--- CP/08/05.cpp
+++ CP/08/05.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	string ans,sc,min,in;
-	getline(cin, ans);
-	getline(cin, sc);
-	getline(cin, min);
-	int c;
-	scanf("%d ",&c);
-	while(c--){
-	  getline(cin, in);
-   	int score=0, tt=0, i=in.length();
-   	while(i--) {
-   		if(in[i] != ' ') {
-   		  tt += sc[i] - 48;
-   		  score += in[i] == ans[i] ? sc[i] - 48 : (in[i] !='.' ? 48-min[i] : 0);
-   	  }
-   	}
-   	int r = ceil(100.0 * score / tt);
-   	cout << (r >= 0 ? r : 0) <<"\n";
-	}
+int main() {
+    string ans,sc,min,in;
+    getline(cin, ans);
+    getline(cin, sc);
+    getline(cin, min);
+    int c;
+    scanf("%d ",&c);
+    while(c--){
+        getline(cin, in);
+        int score=0, tt=0, i=in.length();
+        while(i--) {
+            if(in[i] != ' ') {
+                tt += sc[i] - 48;
+                score += in[i] == ans[i] ? sc[i] - 48 : (in[i] !='.' ? 48-min[i] : 0);
+            }
+        }
+        cout << max((int) ceil(100.0 * score / tt),0) <<"\n";
+    }
 }
