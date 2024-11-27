81d9c6c88e4f29dd6a5932bd679813b68c0e6ef3
speedcubing <git@speedcubing.top>
2024-11-27 23:51:39 +0800

    Update 09.cpp

--- CP/08/09.cpp
+++ CP/08/09.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-    int cnt, size, flag=1,i;
-    while(cin >> cnt >> size){
-      double dist[1024] = {0};
-        if(flag) 
-		      flag = 0;
-        else 
-		      printf("\n");
-        for(i = cnt - 1 ; i ;i--)
+
+int cnt, size, f, i;
+
+int main() {
+	
+    while (cin >> cnt >> size) {
+        double dist[1024] = {};
+      
+        if(f++) cout << "\n";
+         
+        i = cnt;
+        while(i--) {
             dist[i-1] = dist[i] + size / 2.0 / i;
+        }
+        
         for(i = 0;i < cnt;i++) {
-        	int j = floor(dist[i]),cp=size;
-        	while(j--)
-        		printf(" ");
-        	while(cp--)
-			    	printf("*");
-          printf("\n");
+            cout << *(new string(floor(dist[i]),' ')) 
+                 << *(new string(size,'*')) 
+                 <<"\n";
         }
     }
 }
