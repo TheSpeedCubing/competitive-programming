4fcb5c0c57d0b83d97dbbdaa37af5ca22f7ddae1
speedcubing <speedcubing@speedcubing.top>
2026-03-04 18:31:03 +0800

    Update 01.cpp

--- CP/13/01.cpp
+++ CP/13/01.cpp
@@ -2,54 +2,56 @@
 
 using namespace std;
 
-typedef unsigned long long ItsSoFuckingLong;
+typedef unsigned long long LL;
 
-ItsSoFuckingLong P(ItsSoFuckingLong A, ItsSoFuckingLong B) {
-    if(A < B) 
-      return 0;
-      
-    ItsSoFuckingLong t = 1, 
-             i = A - B + 1;
-    while(i <= A) {
-    	 t *= i++;
+LL P(LL A, LL B) {
+    if (A < B)
+        return 0;
+
+    LL t = 1,
+       i = A - B + 1;
+
+    while (i <= A) {
+        t *= i++;
     }
     return t;
 }
 
-ItsSoFuckingLong F(ItsSoFuckingLong N) {
-    if(N == 0) 
-      return 0;
-    
-    vector<ItsSoFuckingLong> digit;
-    
-    ItsSoFuckingLong temp = N;
-    while(temp) {
-    	 digit.push_back(temp % 10);
-    	 temp /= 10;
+LL F(LL N) {
+    if (N == 0)
+        return 0;
+
+    vector<LL> digit;
+
+    LL temp = N;
+    while (temp) {
+        digit.push_back(temp % 10);
+        temp /= 10;
     }
-    
+
     reverse(digit.begin(), digit.end());
-    
+
     bool used[12] = {0};
 
-    ItsSoFuckingLong size = digit.size(), 
-             result = 1, 
-             i = 1;
-    while(i < size) {
-      result += P(10, size - i) - P(9, size - 1 - i);
-    	i++;
+    LL size = digit.size(),
+       result = 1,
+       i = 1;
+
+    while (i < size) {
+        result += P(10, size - i) - P(9, size - 1 - i);
+        i++;
     }
-    
+
     i = 0;
-    while(i < size) {
-        for(int j = 0;j < digit[i];j++) {
-            if((i || j) && !used[j] && 10 > i) 
-              result += P(9 - i, size - i - 1);
+    while (i < size) {
+        for (int j = 0; j < digit[i]; j++) {
+            if ((i || j) && !used[j] && 10 > i)
+                result += P(9 - i, size - i - 1);
         }
-        
-        if(used[digit[i]])
-          break;
-          
+
+        if (used[digit[i]])
+            break;
+
         used[digit[i]] = 1;
         i++;
     }
@@ -57,8 +59,8 @@ ItsSoFuckingLong F(ItsSoFuckingLong N) {
 }
 
 int main() {
-    ItsSoFuckingLong l, r;
-    while(cin >> l >> r) {
+    LL l, r;
+    while (cin >> l >> r) {
         cout << F(r + 1) - F(l) << "\n";
     }
 }
